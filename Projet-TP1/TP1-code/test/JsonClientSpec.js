1d79d8a03a88364dc68c52a587674c6827d52f9c
Coralie <coralie.verrier@etu.utc.fr>
2018-09-20 16:54:45 -0400

    jsonclientSpec

--- Projet-TP1/TP1-code/test/JsonClientSpec.js
+++ Projet-TP1/TP1-code/test/JsonClientSpec.js
@@ -1,17 +1,48 @@
 import JsonClient from '../src/modules/JsonClient.js';
-import sinon from 'sinon';
+// import SharedBox from '../src/sharedbox.js';
 import * as Utils from '../src/Utils/platform.js';
-let assert = require('chai').assert;
+import Response from 'node-fetch';
+// import Blob from 'node-fetch';
+// let assert = require('chai').assert;
+let expect = require('chai').expect;
+let sinon = require('sinon');
 
 let jsonClient;
 
+let fakePromise = function(promiseHelper){
+  return new Promise((resolve, reject) => {
+    promiseHelper(resolve,reject);
+  });
+};
+
+function stubOnCall(jsonResponse){
+  var stub = sinon.stub(Utils,'fetch');
+  // var init = { ok : 200, text: function(){return 'test not null';},json : function(){return jsonResponse;}};
+
+  stub.onFirstCall().returns(fakePromise(r => r(new Response('https://endpoint.random.test/'))));
+  stub.onSecondCall().returns(fakePromise(r => r(new Response(JSON.stringify(jsonResponse)))));
+}
+
 beforeEach(() => {
-  jsonClient = new JsonClient(0,0,0);
+  jsonClient = new JsonClient('token',0,'https://endpoint.random.test/');
 });
 
 it('initializeSharedBox JsonClient test', () => {
-  var stub = sinon.stub(Utils,'fetch').throws();
-  stub.resolves({status:200,statusText:'OK',ok:true,text:function(){return 'coucou';},json:function(){return 'json info';}});
-  var answer = jsonClient.initializeSharedBox('mail');
-  assert.isNull(answer);
+
+  //let reponse = new Response(stubCall());
+  var jsonResponse = {
+    guid: 'dc6f21e0f02c41123b795e4',
+    uploadUrl: 'upload_url'
+  };
+  stubOnCall(jsonResponse);
+
+  return jsonClient.initializeSharedBox('darth.vader@email.com').then(result => {
+    expect(Utils.fetch).to.have.been.calledWith('https://endpoint.random.test/services/sharedbox/server/url/');
+    expect(Utils.fetch).to.have.been.calledWith(
+      'https://endpoint.random.test/api/sharedboxes/new?email=darth.vader@email.com/',
+      { headers: {'Authorization-Token':'token'}, method: 'get' }
+    );
+    // expect(result).to.be.not.null;
+    expect(result.guid).to.be.equal;
+  });
 });
